cb40cc70b53c931fd142d8133667d56a0314d594
Nru707 <nsdanu707@gmail.com>
2024-01-30 22:58:42 +0530

    commit

---
+++ Test Suites/Amazon_Signup_TestSuite/Amazon_Signup_TC001_TestSuite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Amazon_Signup_TC001_TestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>d39d1eba-3ea0-440a-961c-d3f1c45bd921</testSuiteGuid>
+   <testCaseLink>
+      <guid>9f227f70-968f-433f-a331-989e343aca37</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Testing_Modes/By_Record_web/Amazon_signup_TC001</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
